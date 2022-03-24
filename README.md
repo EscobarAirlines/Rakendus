74b9dc62d74317a8780bf49f8e8578737be22dbc
EscobarAirlines <tim.kasper@tptlive.ee>
2022-03-24 11:43:05 +0200

    1

--- README.md
+++ README.md
@@ -2,5 +2,5 @@
 
 This is my first GIT project.
 
-![image](https://raw.githubusercontent.com/EscobarAirlines/Rakendus/main/domestic-cat_thumb_square.jpg)
+![My cat](https://raw.githubusercontent.com/EscobarAirlines/Rakendus/main/domestic-cat_thumb_square.jpg)
 
---
+++ second_tictoe.py
@@ -0,0 +1,23 @@
+game = [
+    ".", ".", ".",
+    ".", ".", ".",
+    ".", ".", "."
+]
+
+print(game[0] + game[1] + game[2])
+print(game[3] + game[4] + game[5])
+print(game[6] + game[7] + game[8])
+
+next_position = input("Please type position: ")
+
+# line 14 and 17 are same. Please make "1" and "2" to int (using int() function and -1)
+if next_position =="1":
+    game[0] = "X"
+
+if next_position =="2":
+    game[1] = "X"
+
+
+print(game[0] + game[1] + game[2])
+print(game[3] + game[4] + game[5])
+print(game[6] + game[7] + game[8])
