d6f9b1af546c740ef12784bfcc2ffd387b5e995f
EscobarAirlines <tim.kasper@tptlive.ee>
2022-03-24 10:54:10 +0200

    Create lesson1.py

---
+++ lesson1.py
@@ -0,0 +1,36 @@
+operation = input("pick action +, -, //, /, *, **, %:")
+
+if operation  == "+":
+    nr1 = int(input("write nr 1:"))
+    nr2 = int(input("write nr 2:"))
+    print(nr1 + nr2)
+
+if operation  == "-":
+    nr1 = int(input("write nr 1:"))
+    nr2 = int(input("write nr 2:"))
+    print(nr1 - nr2)
+
+if operation  == "*":
+    nr1 = int(input("write nr 1:"))
+    nr2 = int(input("write nr 2:"))
+    print(nr1 * nr2)
+
+if operation  == "/":
+    nr1 = int(input("write nr 1:"))
+    nr2 = int(input("write nr 2:"))
+    print(nr1 / nr2)
+
+if operation  == "**":
+    nr1 = int(input("write nr 1:"))
+    nr2 = int(input("write nr 2:"))
+    print(nr1 ** nr2)
+
+if operation  == "//":
+    nr1 = int(input("write nr 1:"))
+    nr2 = int(input("write nr 2:"))
+    print(nr1 // nr2)
+
+if operation  == "%":
+    nr1 = int(input("write nr 1:"))
+    nr2 = int(input("write nr 2:"))
+    print(nr1 % nr2)
