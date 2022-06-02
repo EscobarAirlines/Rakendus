9d8b58fd12f28099146b60ae5919fea2983abdb3
EscobarAirlines <tim.kasper@tptlive.ee>
2022-06-02 10:40:28 +0300

    Update ub.py

--- ub.py
+++ ub.py
@@ -8,55 +8,58 @@ from datetime import datetime
 
 BYTE_TO_MEGABYTE = 0.00000095367432
 
-# weather
 city = 'Tallinn'
 owm = OWM('API KEY')
 manager = owm.weather_manager()
 observation = manager.weather_at_place(city)
 weather = observation.weather
 
-# memory
+
 memory = psutil.virtual_memory()
 memory_total = getattr(memory, 'total') * BYTE_TO_MEGABYTE
 memory_free = getattr(memory, 'free') * BYTE_TO_MEGABYTE
 memory_used = getattr(memory, 'used') 
-memory_limit = (memory_used / 100 * 20) * BYTE_TO_MEGABYTE # 20% of the RAM
+memory_limit = (memory_used / 100 * 20) * BYTE_TO_MEGABYTE
 
-# disk
+
 disk = psutil.disk_usage('/')
 disk_free = getattr(disk, 'total') * BYTE_TO_MEGABYTE
 disk_total = getattr(disk, 'free') * BYTE_TO_MEGABYTE
 disk_used = getattr(disk, 'used') 
-disk_limit = (disk_used / 100 * 20) * BYTE_TO_MEGABYTE # 20% of the disk
+disk_limit = (disk_used / 100 * 20) * BYTE_TO_MEGABYTE
 
-# information about both
+
 def memory_info():
     print('RAM:')
     print('Your total memory: ', round(memory_total, 2), 'MB')
     print('Memory available: ', round(memory_free, 2), 'MB')
 
+
 def disk_info():
     print('\nDisk usage:')
     print('Your total disk memory: ', round(disk_free, 2), 'MB')
     print('Disk memory available: ', round(disk_total, 2), 'MB')
-
-# this block of code is required because Microsoft's cmd uses "cls" instead of "clear"
+    
+    
 if platform.system() == 'Windows':
     os.system('cls')
 else:
     os.system('clear')
 
+
 memory_info()
 disk_info()
 
+
 if memory_used > memory_limit:
-    print('WARNING: You are using more than 20% of your RAM')
+    print('WARNING: Stop this, you are using more than 20% of memory!!!!')
 elif disk_used > disk_limit:
-    print('WARNING: You are using more than 20% of your disk space')
+    print('WARNING: Stop this, you are using more than 20% of disk!!!!!')
+
 
 input()
-    
-# entering history of the progarm in the "history.txt"
+
+
 with open('history.txt', mode='a')as history:
     current_time = str(datetime.now()) 
 
